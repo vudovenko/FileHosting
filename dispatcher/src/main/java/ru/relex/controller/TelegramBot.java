86bfcca480a23a0c0e47b3101c0b408ec56fa061
vudovenko <vladislav.udovenko0@yandex.ru>
2023-03-18 14:54:50 +0500

    added the ability to receive messages through the bot and send him a response

---
+++ dispatcher/src/main/java/ru/relex/controller/TelegramBot.java
@@ -0,0 +1,52 @@
+package ru.relex.controller;
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+@Component
+@Log4j
+public class TelegramBot extends TelegramLongPollingBot {
+    @Value("${bot.name}")
+    private String botName;
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var originalMessage = update.getMessage();
+        log.debug(originalMessage.getText()); //TODO почему-то не логируется при debug...
+        log.info(originalMessage.getText());
+
+        var response = new SendMessage();
+        response.setChatId(originalMessage.getChatId().toString());
+        response.setText("Hello from bot");
+        sendAnswerMessage(response);
+    }
+
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e);
+            }
+        }
+    }
+}
