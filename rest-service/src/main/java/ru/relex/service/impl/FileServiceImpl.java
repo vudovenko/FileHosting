1a38c2b3a6f16e0eb212c21cc8fbf56c7182755d
vudovenko <vladislav.udovenko0@yandex.ru>
2023-10-01 19:48:57 +0500

    ref

--- rest-service/src/main/java/ru/relex/service/impl/FileServiceImpl.java
+++ rest-service/src/main/java/ru/relex/service/impl/FileServiceImpl.java
@@ -2,20 +2,14 @@ package ru.relex.service.impl;
 
 
 import lombok.extern.log4j.Log4j;
-import org.apache.commons.io.FileUtils;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.relex.entity.AppDocument;
 import ru.relex.entity.AppPhoto;
-import ru.relex.entity.BinaryContent;
 import ru.relex.repository.AppDocumentRepository;
 import ru.relex.repository.AppPhotoRepository;
 import ru.relex.service.FileService;
 import ru.relex.utils.CryptoTool;
-
-import java.io.File;
-import java.io.IOException;
 
 /**
  * Отечает за получение файлов из БД
@@ -52,29 +46,5 @@ public class FileServiceImpl implements FileService {
             return null;
         }
         return appPhotoRepository.findById(id).orElse(null);
-    }
-
-    /**
-     * Преобразовывает массив байт из БД в объект класса FileSystemResource,
-     * который можно отправить в теле ответа пользователю.
-     */
-    @Override
-    public FileSystemResource getFileSystemResource(BinaryContent binaryContent) {
-        try {
-            // TODO добавить генерацию имени временного файла
-            // Создаем временный файл с расширением .bin
-            File temp = File.createTempFile("tempFile", ".bin");
-            // При завершении JVM удаляем файл из постоянной памяти компьютера.
-            // По-сути метод регистрирует файл в очередь на удаление
-            temp.deleteOnExit();
-            // Записываем массив байт в объект временного файла.
-            FileUtils.writeByteArrayToFile(temp, binaryContent.getFileAsArrayOfBytes());
-            // Оборачиваем файл в объект FileSystemResource,
-            // который может быть передан в теле ответа HTTP.
-            return new FileSystemResource(temp);
-        } catch (IOException e) {
-            log.error(e);
-            return null;
-        }
     }
 }
